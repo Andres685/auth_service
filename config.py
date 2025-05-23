45d610dfc9545a03f0bf8e5c3bc7177b0626daae
Andres Guzman <guzmanandres685gmail.com>
2025-05-22 19:37:42 -0500

    s

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://Adminbd:Admin123*@serversqlazureag.database.windows.net:1433/PeliculasFlask?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes'
+    SQLALCHEMY_DATABASE_URI = 'mssql://Adminbd:Admin123*@serversqlazureag.database.windows.net:1433/PeliculasFlask?driver=ODBC+Driver+18+for+SQL+Server&Encrypt=yes'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
